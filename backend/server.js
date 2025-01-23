6a79f6c029bc90c3a6f0bfd52a83513166e27c3b
Brijesh Poojary <brijeshpujari333@gmail.com>
2025-01-23 09:38:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,6 @@ app.use(cors(
         credentials: true,
     }
 ));
-app.use(cors())
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 app.use('/api/v1/candidates', candidateRoutes);

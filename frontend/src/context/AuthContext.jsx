3039620cd74b9538e284a60905544025a08b32d6
Brijesh Poojary <brijeshpujari333@gmail.com>
2025-01-29 15:53:58 +0530

    Merge branch 'main' of https://github.com/brijesh1802/HireNest

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,19 +1,18 @@
 import { createContext, useState } from "react";
 
-export const AuthContext=createContext();
-export const AuthProvider=({children})=>{
-    const [userType,setUserType]=useState(null);
-    const login=(type)=>{
-        setUserType(type)
-    }
-    const logout=()=>{
-        setUserType(null);
-
-        localStorage.clear();
-    }
-    return(
-        <AuthContext.Provider value={{userType,login,logout}}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+export const AuthContext = createContext();
+export const AuthProvider = ({ children }) => {
+  const [userType, setUserType] = useState(null);
+  const login = (type) => {
+    setUserType(type);
+  };
+  const logout = () => {
+    setUserType(null);
+    localStorage.clear();
+  };
+  return (
+    <AuthContext.Provider value={{ userType, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};

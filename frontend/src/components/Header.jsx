929bda909dcb1f3336699c9fcba6cf00f4a2d184
Chayanka <chayanka2318@gmail.com>
2025-02-20 10:20:24 +0530

    Integrated change password, dynamic loading, and delete account options.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,6 +4,10 @@ import { AuthContext } from "../context/AuthContext";
 import Logo from "../assets/Logo.png";
 import { FaBars, FaTimes, FaChevronDown, FaChevronUp } from "react-icons/fa";
 import default_img from "../assets/default_image.png";
+import LogoutConfirmation from "./LogoutConfirmation";
+import ChangePasswordConfirmation from "./ChangePasswordConfirmation";
+import Loading from "./Loading";
+
 function Header() {
   const { userType, logout } = useContext(AuthContext);
   const navigate = useNavigate();
@@ -11,9 +15,38 @@ function Header() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [moreOpen, setMoreOpen] = useState(false);
   const [manageProfileOpen, setManageProfileOpen] = useState(false);
-  const dropdownRef = useRef(null);
+  const [logoutConfirmOpen, setLogoutConfirmOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [changePasswordConfirmOpen, setChangePasswordConfirmOpen] =
+    useState(false);
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
   const [userData, setUserData] = useState(null);
+  const dropdownRef = useRef(null);
+  const sideNavRef = useRef(null);
+
+  const handleSubmit = async () => {
+    const email = userData.email;
+    if (!email) return;
+
+    const url = `${
+      import.meta.env.VITE_BASE_URL
+    }/api/v1/auth/request-password-reset`;
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      const data = await response.json();
+      console.log("Response Data:", data);
+    } catch (err) {
+      console.error("Network error:", err);
+    }
+  };
 
   const handleLogout = () => {
     logout();
@@ -21,17 +54,53 @@ function Header() {
     setNavOpen(false);
   };
 
+  const handleLogoutClick = () => {
+    setLogoutConfirmOpen(true);
+  };
+
+  const handleLogoutConfirm = () => {
+    setLoading(true);
+    setLogoutConfirmOpen(false);
+    setTimeout(() => {
+      handleLogout(); // Call your logout function
+
+      setLoading(false); // Hide loading after logout
+    }, 3000);
+  };
+
+  const handleLogoutCancel = () => {
+    setLogoutConfirmOpen(false);
+  };
+
+  const handleChangePasswordClick = () => {
+    setChangePasswordConfirmOpen(true);
+  };
+
+  const handleChangePasswordConfirm = () => {
+    handleSubmit();
+    handleLogout();
+    navigate("/login");
+    setChangePasswordConfirmOpen(false);
+  };
+
+  const handleChangePasswordCancel = () => {
+    setChangePasswordConfirmOpen(false);
+  };
+
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth < 768);
       if (window.innerWidth >= 768) {
         setNavOpen(false);
+        setDropdownOpen(false);
+        setMoreOpen(false);
+        setManageProfileOpen(false);
       }
     };
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
-  /*fetch data*/
+
   useEffect(() => {
     const fetchUserData = async () => {
       try {
@@ -59,43 +128,34 @@ function Header() {
       }
     };
 
-    if (userType) fetchUserData();
+    if (userType && !userData) fetchUserData();
   }, [userType]);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
+      if (sideNavRef.current && !sideNavRef.current.contains(event.target)) {
+        setNavOpen(false);
+        setDropdownOpen(false);
+        setMoreOpen(false);
+        setManageProfileOpen(false);
+      }
+
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
         setDropdownOpen(false);
-        setManageProfileOpen(false);
       }
     };
 
-    const handleResize = () => {
-      setDropdownOpen(false);
-      setManageProfileOpen(false);
-    };
-
     document.addEventListener("mousedown", handleClickOutside);
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-      window.removeEventListener("resize", handleResize);
-    };
+    return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
-  console.log(userData);
-  useEffect(() => {
-    if (navOpen) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
-    }
 
+  useEffect(() => {
+    document.body.style.overflow = navOpen ? "hidden" : "auto";
     return () => {
       document.body.style.overflow = "auto";
     };
   }, [navOpen]);
-  console.log(userData?.profilePhoto);
+
   return (
     <header className="fixed z-10 w-full bg-white shadow-md">
       <div className="flex items-center justify-between px-6 py-4">
@@ -145,23 +205,11 @@ function Header() {
             </NavLink>
           )}
           <div className="relative" ref={dropdownRef}>
-            {userType === "candidate" && userData ? (
+            {userType && userData ? (
               <button
                 onClick={() => setDropdownOpen(!dropdownOpen)}
                 className="flex items-center focus:outline-none"
               >
-                <img
-                  src={userData.profilePhoto || default_img}
-                  alt="Profile"
-                  className="object-cover w-10 h-10 rounded-full"
-                />
-              </button>
-            ) : userType === "employer" && userData ? (
-              <button
-                onClick={() => setDropdownOpen(!dropdownOpen)}
-                className="flex items-center focus:outline-none"
-              >
-                {/* <span className="text-lg text-gray-700">{userData?.name}</span> */}
                 <img
                   src={userData.profilePhoto || default_img}
                   alt="Profile"
@@ -180,7 +228,7 @@ function Header() {
               </button>
             )}
             {dropdownOpen && (
-              <div className="absolute right-0 w-48 py-2 mt-2 bg-white rounded-md shadow-lg">
+              <div className="absolute right-0 min-w-max py-2 mt-2 bg-white rounded-md shadow-lg">
                 <button
                   className="w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-100 flex justify-between items-center"
                   onClick={() => setManageProfileOpen(!manageProfileOpen)}
@@ -200,11 +248,31 @@ function Header() {
                     </NavLink>
                     {userType === "candidate" && (
                       <NavLink
-                        to="/application"
+                        to="/applications"
                         className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
                         onClick={() => setDropdownOpen(false)}
                       >
                         My Application
+                      </NavLink>
+                    )}
+                    <NavLink
+                      to="delete-account"
+                      className="block w-full px-4 py-2 text-left text-gray-800 hover:bg-gray-100"
+                      onClick={() => setDropdownOpen(false)}
+                    >
+                      Delete My Account
+                    </NavLink>
+                    {userData?.modeofLogin === "email" && (
+                      <NavLink
+                        onClick={() => {
+                          setDropdownOpen(false);
+                          {
+                            handleChangePasswordClick();
+                          }
+                        }}
+                        className="block w-full px-4 py-2 text-left text-gray-800 hover:bg-gray-100"
+                      >
+                        Change Password
                       </NavLink>
                     )}
                   </div>
@@ -219,10 +287,7 @@ function Header() {
                 </NavLink>
 
                 <button
-                  onClick={() => {
-                    handleLogout();
-                    setDropdownOpen(false);
-                  }}
+                  onClick={handleLogoutClick}
                   className="block w-full px-4 py-2 text-left text-gray-800 hover:bg-gray-100"
                 >
                   Log Out
@@ -235,7 +300,12 @@ function Header() {
 
       {navOpen && (
         <div className="fixed inset-0 z-20 bg-gray-700 bg-opacity-50">
-          <div className="fixed top-0 left-0 z-30 h-full bg-white shadow-lg w-72">
+          <div
+            ref={sideNavRef}
+            className={`fixed top-0 left-0 z-30 h-full bg-white shadow-lg w-72 transform ${
+              navOpen ? "translate-x-0" : "-translate-x-full"
+            } transition-transform duration-300 ease-in-out overflow-y-auto max-h-screen`}
+          >
             <button
               onClick={() => setNavOpen(false)}
               className="absolute text-gray-700 top-4 right-4"
@@ -244,26 +314,24 @@ function Header() {
             </button>
 
             <div className="flex flex-col items-start px-4 py-6">
-              {userType === "candidate" && userData ? (
-                <div className="flex flex-col ml-2 text-sm">
+              {userType && userData ? (
+                <div
+                  className="flex flex-col ml-2 text-sm hover:cursor-pointer"
+                  onClick={() => {
+                    navigate("/profile");
+                    setNavOpen(false);
+                  }}
+                >
                   <img
                     src={userData.profilePhoto || default_img}
                     className="object-cover w-10 h-10 rounded-full"
                   />
                   <span className="flex items-start">
-                    {userData?.fullName?.firstName || "Unknown"}
+                    {userData?.fullName?.firstName ||
+                      userData?.name ||
+                      "Unknown"}
                   </span>
                   <span>{userData?.email || "No email provided"}</span>
-                  <hr className="w-full my-2 border-gray-300" />
-                </div>
-              ) : userType === "employer" && userData ? (
-                <div className="flex flex-col ml-2 text-sm">
-                  <img
-                    src={userData.profilePhoto || default_img}
-                    className="object-cover w-10 h-10 rounded-full"
-                  />
-                  <span className="flex items-start">{userData.name}</span>
-                  <span>{userData.email}</span>
                   <hr className="w-full my-2 border-gray-300" />
                 </div>
               ) : null}
@@ -276,14 +344,19 @@ function Header() {
                 Home
               </NavLink>
 
-              {userType === "candidate" && (
+              {userType === "candidate" && userData != null ? (
                 <NavLink
-                  to="/application"
+                  to="/applications"
                   className="py-2 text-lg hover:text-orange-500"
-                  onClick={() => setNavOpen(false)}
+                  onClick={() => {
+                    navigate("/applications");
+                    setNavOpen(false);
+                  }}
                 >
                   My Application
                 </NavLink>
+              ) : (
+                ""
               )}
 
               {userType === "candidate" || userType === null ? (
@@ -317,35 +390,51 @@ function Header() {
                   <NavLink
                     to="/safety"
                     className="block py-2 text-lg hover:text-orange-500"
-                    onClick={() => setNavOpen(false)}
+                    onClick={() => {
+                      setNavOpen(false);
+                      setMoreOpen(false);
+                    }}
                   >
                     Safety Tips
                   </NavLink>
 
-                  <button
-                    className="flex items-center justify-between w-full py-2 text-lg hover:text-orange-500"
-                    onClick={() => setManageProfileOpen(!manageProfileOpen)}
-                  >
-                    Manage Profile{" "}
-                    {manageProfileOpen ? <FaChevronUp /> : <FaChevronDown />}
-                  </button>
+                  {userData !== null ? (
+                    <button
+                      className="flex items-center justify-between w-full py-2 text-lg hover:text-orange-500"
+                      onClick={() => setManageProfileOpen(!manageProfileOpen)}
+                    >
+                      Manage Profile
+                      {manageProfileOpen ? (
+                        <FaChevronUp />
+                      ) : (
+                        <FaChevronDown className="ml-10" />
+                      )}
+                    </button>
+                  ) : (
+                    ""
+                  )}
 
                   {manageProfileOpen && (
-                    <div className="pl-4">
+                    <div className="w-48 pl-2">
                       <NavLink
                         to="/delete-account"
-                        className="block py-2 text-lg hover:text-orange-500"
-                        onClick={() => setNavOpen(false)}
+                        className="block py-2 text-lg hover:text-orange-500 w-full text-left"
+                        onClick={() => {
+                          setNavOpen(false);
+                          setManageProfileOpen(false);
+                          setMoreOpen(false);
+                        }}
                       >
                         Delete My Account
                       </NavLink>
-                      <NavLink
-                        to="/change-password"
-                        className="block py-2 text-lg hover:text-orange-500"
-                        onClick={() => setNavOpen(false)}
-                      >
-                        Change Password
-                      </NavLink>
+                      {userData?.modeofLogin === "email" && (
+                        <button
+                          className="block py-2 text-lg hover:text-orange-500"
+                          onClick={handleChangePasswordClick}
+                        >
+                          Change Password
+                        </button>
+                      )}
                     </div>
                   )}
                 </div>
@@ -354,7 +443,7 @@ function Header() {
 
               {userData !== null ? (
                 <button
-                  onClick={handleLogout}
+                  onClick={handleLogoutClick}
                   className="py-2 text-lg hover:text-orange-500"
                 >
                   Log Out
@@ -381,6 +470,22 @@ function Header() {
           </div>
         </div>
       )}
+
+      {logoutConfirmOpen && (
+        <LogoutConfirmation
+          onConfirm={handleLogoutConfirm}
+          onCancel={handleLogoutCancel}
+        />
+      )}
+
+      {changePasswordConfirmOpen && (
+        <ChangePasswordConfirmation
+          onConfirm={handleChangePasswordConfirm}
+          onCancel={handleChangePasswordCancel}
+        />
+      )}
+
+      {loading && <Loading />}
     </header>
   );
 }

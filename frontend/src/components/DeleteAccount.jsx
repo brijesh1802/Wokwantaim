fc8dffbceec97bc588dc0fa52858be6fcf958ef3
Chayanka <chayanka2318@gmail.com>
2025-02-24 09:20:23 +0530

    integrated delete account

--- frontend/src/components/DeleteAccount.jsx
+++ frontend/src/components/DeleteAccount.jsx
@@ -3,12 +3,15 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import { Trash2 } from "lucide-react";
 import { FaExclamationTriangle } from "react-icons/fa";
+import Loading from "./Loading";
 
-const DeleteAccount = () => {
+const DeleteAccount = ({ onClose }) => {
   const [step, setStep] = useState(1);
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
   const { userType } = useContext(AuthContext);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const navigate = useNavigate();
   const token = localStorage.getItem("authToken");
   const userEmail = user?.email || "user@example.com"; // Simulated user email
@@ -49,16 +52,57 @@ const DeleteAccount = () => {
     };
   }, []);
 
-  const handleDelete = () => {
-    if (password === "test123") {
-      alert("Account deleted successfully!");
-      navigate("/"); // Redirect home after deletion
-    } else {
-      alert("Incorrect password!");
+  const handleDelete = async () => {
+    if (user.modeofLogin === "email" && !password) {
+      setError("Password cannot be empty.");
+      return;
+    }
+
+    const url =
+      userType === "candidate"
+        ? `${import.meta.env.VITE_BASE_URL}/api/v1/candidates/deleteProfile`
+        : `${import.meta.env.VITE_BASE_URL}/api/v1/employers/deleteProfile`;
+
+    try {
+      setLoading(true);
+      const response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ password }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        setError(errorData.msg || "An error occurred. Please try again.");
+        setLoading(false);
+        return;
+      }
+
+      const data = await response.json();
+      console.log("Response Data:", data);
+      localStorage.removeItem("authToken");
+      setTimeout(() => {
+        setLoading(false);
+        onClose(); // Close the modal
+        window.location.reload();
+        navigate("/"); // Redirect to home page after deletion
+      }, 2000);
+    } catch (err) {
+      console.error("Network error:", err);
+      setError(
+        err.message ||
+          "An error occurred while deleting the account. Please try again."
+      );
+      setLoading(false);
     }
   };
 
   return (
+    <>
+    {loading?(<Loading/>):(
     <div className="fixed inset-0 z-20 flex items-center justify-center bg-black bg-opacity-50">
       {/* Modal Container (Scrollable) */}
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-lg max-h-[90vh] overflow-y-auto">
@@ -76,14 +120,13 @@ const DeleteAccount = () => {
                 <FaExclamationTriangle className="h-6 w-6 text-yellow-400 flex-shrink-0" />
                 <p className="ml-4 text-base text-yellow-700">
                   This action is <strong>permanent</strong> and cannot be
-                  undone. All your data, including saved preferences, will be
                   lost.
                 </p>
               </div>
             </div>
             <div className="flex justify-end space-x-4 mt-6">
               <button
-                onClick={() => navigate("/")}
+                onClick={onClose}
                 className="bg-gray-500 text-white px-5 py-2 rounded-lg shadow-md 
                 hover:bg-gray-600 hover:scale-105 transition-transform duration-300"
               >
@@ -103,44 +146,79 @@ const DeleteAccount = () => {
         {/* Step 2: Enter Password */}
         {step === 2 && (
           <>
-            <h2 className="text-2xl font-medium text-center text-gray-800">
-              Enter Password
-            </h2>
-            <p className="mt-4 text-sm text-yellow-700 bg-yellow-50 p-3 rounded-lg border-l-4 border-yellow-500">
-              🔒 <strong>For security reasons</strong>, please confirm your
-              identity by entering your password.
-            </p>
-            <p className="mt-3 text-md font-semibold text-gray-900">
-              {userEmail}
-            </p>
-            <input
-              type="password"
-              placeholder="Enter password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md mt-4 bg-gray-100 text-gray-900 focus:outline-none focus:ring-2 focus:ring-red-500"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <div className="flex justify-end space-x-4 mt-6">
-              <button
-                onClick={() => navigate("/")}
-                className="bg-gray-500 text-white px-5 py-2 rounded-lg shadow-md 
+            {user.modeofLogin === "email" ? (
+              <>
+                <h2 className="text-2xl font-medium text-center text-gray-800">
+                  Enter Password
+                </h2>
+                <p className="mt-4 text-sm text-yellow-700 bg-yellow-50 p-3 rounded-lg border-l-4 border-yellow-500">
+                  🔒 <strong>For security reasons</strong>, please confirm your
+                  identity by entering your password.
+                </p>
+                <p className="mt-3 text-md font-semibold text-gray-900">
+                  {userEmail}
+                </p>
+
+                <input
+                  type="password"
+                  placeholder="Enter password"
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md mt-4 bg-gray-100 text-gray-900 focus:outline-none focus:ring-2 focus:ring-red-500"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {error && <p className="mt-2 text-sm text-red-600">{error}</p>}
+                <div className="flex justify-end space-x-4 mt-6">
+                  <button
+                    onClick={onClose}
+                    className="bg-gray-500 text-white px-5 py-2 rounded-lg shadow-md 
                 hover:bg-gray-600 hover:scale-105 transition-transform duration-300"
-              >
-                Cancel
-              </button>
-              <button
-                onClick={handleDelete}
-                className="bg-red-700 text-white px-5 py-2 rounded-lg shadow-md 
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={handleDelete}
+                    className="bg-red-700 text-white px-5 py-2 rounded-lg shadow-md 
                 hover:bg-red-800 hover:scale-105 transition-transform duration-300 flex items-center space-x-2"
-              >
-                <Trash2 />
-                <span>Yes, Confirm Delete</span>
-              </button>
-            </div>
+                  >
+                    <Trash2 />
+                    <span>Yes, Confirm Delete</span>
+                  </button>
+                </div>
+              </>
+            ) : (
+              <>
+                <h2 className="text-2xl font-medium text-center text-gray-800">
+                  Confirm Deletion
+                </h2>
+                <p className="mt-4 text-sm text-yellow-700 bg-yellow-50 p-3 rounded-lg border-l-4 border-yellow-500">
+                  🔒 Confirm your action by clicking{" "}
+                  <strong>Yes, Confirm Delete</strong>
+                </p>
+                <div className="flex justify-end space-x-4 mt-6">
+                  <button
+                    onClick={onClose}
+                    className="bg-gray-500 text-white px-5 py-2 rounded-lg shadow-md 
+                hover:bg-gray-600 hover:scale-105 transition-transform duration-300"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={handleDelete}
+                    className="bg-red-700 text-white px-5 py-2 rounded-lg shadow-md 
+                hover:bg-red-800 hover:scale-105 transition-transform duration-300 flex items-center space-x-2"
+                  >
+                    <Trash2 />
+                    <span>Yes, Confirm Delete</span>
+                  </button>
+                </div>
+              </>
+            )}
           </>
         )}
       </div>
     </div>
+    )}
+    </>
   );
 };
 
